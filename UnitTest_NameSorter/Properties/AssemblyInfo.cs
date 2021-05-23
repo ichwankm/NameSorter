9c5a50f6887c40c4d9313e0342c904fa986254bd
ichwankm <ichwankm@gmail.com>
2021-05-23 15:37:24 +0700

    Add project files.

---
+++ UnitTest_NameSorter/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_NameSorter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_NameSorter")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c2fead2-15ab-4dd8-8721-d00b30cc458c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest_NameSorter/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Name_Sorter;
+
+namespace UnitTest_NameSorter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodExecuteProgram()
+        {
+            string input = "name-sorter ./unsorted-names-list.txt";
+
+            var output = SortingName.ExecuteCommand(input);
+
+            var expectedoutput = false;
+
+            Assert.AreEqual(expectedoutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethodOrderList()
+        {
+            string input = "Ichwan Kurnia Meidiandrianto";
+
+            var output = SortingName.OrderbyLastName(input);
+
+            var expectedoutput = "Meidiandrianto";
+
+            Assert.AreEqual(expectedoutput, output);
+        }
+    }
+}
